924cb7c2a12f220c27155034ef89a50ea34680d5
Alvaro <al7aro@gmail.com>
2026-02-02 11:44:23 +0100

    init script

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -1,4 +1,10 @@
 #!/bin/bash
 
-# DOWNLOAD GLFW REPO
-git clone -b 3.3-stable https://github.com/glfw/glfw.git ../Feldespato/vendor/glfw/
+SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
+
+# DOWNLOAD GLFW
+git clone -b 3.3-stable https://github.com/glfw/glfw.git "$SCRIPT_DIR/../Feldespato/vendor/glfw/glfw/"
+
+mkdir -p "${SCRIPT_DIR}/../build"
+
+cmake -S "${SCRIPT_DIR}/.." -B "${SCRIPT_DIR}/../build"
